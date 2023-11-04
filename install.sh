72c8c4981172d826e9dc0f0efac43ab5f28fdbd2
Baba01hacker666 <117832562+Baba01hacker666@users.noreply.github.com>
2023-11-04 17:22:28 +0545

    Update install.sh
    Making scrpit work better

--- install.sh
+++ install.sh
@@ -1,15 +1,21 @@
 RED='\033[0;31m'
-printf "${RED}*********Installing Nikto**********\n"
-if ! test -f //data/data/com.termux/files/home/tools; then
-  mkdir  tools
+
+# Check if the 'tools' directory exists and create it if not
+if ! test -d $HOME/tools; then
+  mkdir $HOME/tools
 fi
-cd tools
-if ! test -f /data/data/com.termux/files/home/tools/nikto/; then
-  git clone  https://github.com/sullo/nikto
+
+# Change to the 'tools' directory
+cd $HOME/tools
+
+# Check if Nikto is already installed, and if not, install it
+if ! test -d $HOME/tools/nikto; then
+  git clone https://github.com/sullo/nikto.git
   cd nikto/program
   cp nikto.pl nikto
   mv nikto $PREFIX/bin
   cd $PREFIX/bin
   wget https://raw.githubusercontent.com/Baba01hacker666/Install-nikto/main/nikto.conf
 fi
+
 printf "${RED}*********Installed with no error*********"
